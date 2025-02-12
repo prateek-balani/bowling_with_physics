af5a677d695a0b38e533f42d399847863d87af00
prateek-balani <prateekbalani03@gmail.com>
2025-02-12 11:38:19 -0800

    ballcontroller added

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] private float force = 1f;
+    [SerializeField] private Transform ballAnchor;
+    private bool isBallLaunched;
+    private Rigidbody ballRB;
+    [SerializeField] private InputManager inputManager;
+
+    void Start()
+    {
+        ballRB = GetComponent<Rigidbody>();
+        inputManager.OnSpacePressed.AddListener(LaunchBall);
+        transform.parent = ballAnchor;
+        transform.localPosition = Vector3.zero;
+        ballRB.isKinematic = true;
+    }
+
+    private void LaunchBall()
+    {
+        if (isBallLaunched) return;
+
+        isBallLaunched = true;
+        transform.parent = null;
+        ballRB.isKinematic = false;
+        ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
+    }
+
+    public void ResetBall()
+    {
+        isBallLaunched = false;
+        ballRB.isKinematic = true;
+        transform.parent = ballAnchor;
+        transform.localPosition = Vector3.zero;
+    }
+}
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,13 +1,11 @@
-using System;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class InputManager : MonoBehaviour
 {
-    
-    public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
     public UnityEvent OnSpacePressed = new UnityEvent();
-    
+    public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
+    public UnityEvent OnResetPressed = new UnityEvent();
 
     void Update()
     {
@@ -17,15 +15,13 @@ public class InputManager : MonoBehaviour
         }
 
         Vector2 input = Vector2.zero;
-        if (Input.GetKey(KeyCode.A)){ 
-            input += Vector2.left;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            input += Vector2.right;
-        }
-
+        if (Input.GetKey(KeyCode.A)) input += Vector2.left;
+        if (Input.GetKey(KeyCode.D)) input += Vector2.right;
         OnMove?.Invoke(input);
 
-        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            OnResetPressed?.Invoke();
+        }
     }
 }
